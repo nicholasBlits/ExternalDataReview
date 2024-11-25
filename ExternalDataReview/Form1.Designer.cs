773be37db279601ef2cbcd9d26f0be1c3d166021
nblits <nblits@LE004-03>
2024-11-25 10:47:43 -0600

    Add project files.

---
+++ ExternalDataReview/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace ExternalDataReview
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbID = new System.Windows.Forms.TextBox();
+            this.tbFirstName = new System.Windows.Forms.TextBox();
+            this.tbLastName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.dgvStudents = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbMajor = new System.Windows.Forms.TextBox();
+            this.btnViewStudents = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudents)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbID
+            // 
+            this.tbID.Location = new System.Drawing.Point(118, 29);
+            this.tbID.Name = "tbID";
+            this.tbID.Size = new System.Drawing.Size(100, 20);
+            this.tbID.TabIndex = 0;
+            // 
+            // tbFirstName
+            // 
+            this.tbFirstName.Location = new System.Drawing.Point(118, 84);
+            this.tbFirstName.Name = "tbFirstName";
+            this.tbFirstName.Size = new System.Drawing.Size(100, 20);
+            this.tbFirstName.TabIndex = 1;
+            // 
+            // tbLastName
+            // 
+            this.tbLastName.Location = new System.Drawing.Point(118, 150);
+            this.tbLastName.Name = "tbLastName";
+            this.tbLastName.Size = new System.Drawing.Size(100, 20);
+            this.tbLastName.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "First Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Last Name:";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(143, 256);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 6;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // dgvStudents
+            // 
+            this.dgvStudents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStudents.Location = new System.Drawing.Point(259, 29);
+            this.dgvStudents.Name = "dgvStudents";
+            this.dgvStudents.Size = new System.Drawing.Size(404, 193);
+            this.dgvStudents.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 209);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Major:";
+            // 
+            // tbMajor
+            // 
+            this.tbMajor.Location = new System.Drawing.Point(118, 209);
+            this.tbMajor.Name = "tbMajor";
+            this.tbMajor.Size = new System.Drawing.Size(100, 20);
+            this.tbMajor.TabIndex = 8;
+            // 
+            // btnViewStudents
+            // 
+            this.btnViewStudents.Location = new System.Drawing.Point(409, 256);
+            this.btnViewStudents.Name = "btnViewStudents";
+            this.btnViewStudents.Size = new System.Drawing.Size(133, 23);
+            this.btnViewStudents.TabIndex = 10;
+            this.btnViewStudents.Text = "View Students";
+            this.btnViewStudents.UseVisualStyleBackColor = true;
+            this.btnViewStudents.Click += new System.EventHandler(this.btnViewStudents_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(693, 304);
+            this.Controls.Add(this.btnViewStudents);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbMajor);
+            this.Controls.Add(this.dgvStudents);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbLastName);
+            this.Controls.Add(this.tbFirstName);
+            this.Controls.Add(this.tbID);
+            this.Name = "Form1";
+            this.Text = "Students";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudents)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbID;
+        private System.Windows.Forms.TextBox tbFirstName;
+        private System.Windows.Forms.TextBox tbLastName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.DataGridView dgvStudents;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbMajor;
+        private System.Windows.Forms.Button btnViewStudents;
+    }
+}
+
---
+++ ExternalDataReview/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExternalDataReview
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = @"server=(localdb)\MSSQLLocalDB;database=DU;Trusted_connection=true";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            Student student = new Student();
+            student.Id = int.Parse(tbID.Text);
+            student.FirstName = tbFirstName.Text;
+            student.LastName = tbLastName.Text;
+            student.Major = tbMajor.Text;
+            AddStudentToDb(student);
+        }
+
+        public void AddStudentToDb(Student student)
+        {
+            //Open up the SQL Connection
+            //Insert into my Student Table
+            //*INSERTION QUERY*
+            //INSERT INTO dbo.Student(Id, FirstName, LastName, Major)
+            //VALUES(@Id, @FirstName, @LastName, @Major) <-- With the @ sign are called "parameters" and they can be thought of as variables
+            //below, populate these parameters
+
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+                string query = "INSERT INTO dbo.Student(Id, FirstName, LastName, Major)" + "VALUES(@Id, @FirstName, @LastName, @Major)";
+
+                using (SqlCommand sql = new SqlCommand(query, sqlConnection))
+                {
+                    sql.Parameters.AddWithValue("@Id", student.Id);
+                    sql.Parameters.AddWithValue("@FirstName", student.FirstName);
+                    sql.Parameters.AddWithValue("@LastName", student.LastName);
+                    sql.Parameters.AddWithValue("@Major", student.Major);
+                    sql.ExecuteNonQuery();
+                }
+            }
+
+        }
+
+        private void btnViewStudents_Click(object sender, EventArgs e)
+        {
+            //Open up a SQL Connection
+            //Populate my DataTable with the data I get from my Select Query
+            //Populate my DataGridView with my DataTable
+
+            using (SqlConnection newSqlConnection = new SqlConnection(connectionString))
+            {
+                newSqlConnection.Open();
+                string query = "SELECT * FROM dbo.Student";
+
+                using (SqlCommand newSql = new SqlCommand(query, newSqlConnection))
+                {
+                    SqlDataAdapter adapter = new SqlDataAdapter(query, newSqlConnection);
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+
+                    dgvStudents.DataSource = dt;
+                }
+            }
+        }
+    }
+}
