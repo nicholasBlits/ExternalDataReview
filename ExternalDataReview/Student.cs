336ff4608c9eccbf37db561b68324d5bccd6b303
Nicholas Blits <nicholasblits05@yahoo.com>
2024-11-25 11:02:36 -0600

    Added some comments

--- ExternalDataReview/Student.cs
+++ ExternalDataReview/Student.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ExternalDataReview.Properties;
 
 namespace ExternalDataReview
 {
     public class Student
     {
+        // Many student properties
+
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
